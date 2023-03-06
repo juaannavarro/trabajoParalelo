4181199d49da6dd26e2c92f61c350208a643e377
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2023-03-06 17:12:12 +0100

    zzz

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     while True:
         print("1. Paralelo")
         print("2. Secuencial")
+        print("3. Comparar")
+        print("4. Salir")
         opcion = input("Ingrese una opcion: ")
         if opcion == "1":
             start= time.time()
@@ -14,22 +16,30 @@ if __name__ == "__main__":
             pool.close()    #cerramos el pool de procesos.
             print()
             for row in data:   #imprimimos data,(las urls y el tiempo que tarda en rasparlas).
-                    print((row))
-            end = time.time()
+                print((row))
+            end = time.time()         
             print("Total time taken:", end - start, "seconds")
+            def guardar_paralelo():
+                tiempo = end - start
+                print("El tiempo total de ejecucion en paralelo es: ", tiempo)
             
 
         elif opcion == "2":
-            start= time.time()
+            start1= time.time()
             output = []
             for url in urls:
                 result = scrape(url)
             output.append(result)
+            end1 = time.time()
+            print("Total time taken:", end1 - start1, "seconds")
+            def guardar_secuencial():
+                tiempo1 = end1 - start1
+                print("El tiempo total de ejecucion en secuencial es: ", tiempo1)
             
-            end = time.time()
-            print("Total time taken:", end - start, "seconds")
-            
-            
+        elif opcion == "3":
+            print("Comparar")
+            guardar_paralelo()
+            guardar_secuencial()
         else:
             print("Opcion invalida")
             break
